272277f83f6bb45c124ea4d7a6de3d071b25d0ce
sorie <enne3939@gmail.com>
2018-10-04 10:36:24 +0900

    todolist 추가 및 바인딩

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Example from '@/components/Example'
+import TodoPage from '@/components/TodoPage'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/example',
+      name: 'Example',
+      component: Example
+    },
+    {
+      path: '/todos',
+      name: 'TodoPage',
+      component: TodoPage
     }
   ]
 })
